0c1da7b7adced16f423a895e1894c76cc75af8cc
Vanessa Maldonado <vbarajas4@gmail.com>
2021-07-17 08:52:02 -0700

    setting up server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true, //being used to avoid errors as old version is being worked on
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+app.use(require("") )
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
