e0ec06959e809288639b0b56ca9e981bc0b8590c
Vanessa Maldonado <vbarajas4@gmail.com>
2021-07-20 15:32:49 -0700

    schema models

---
+++ models/Workout.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+
+            type: {
+                type: String,
+                trim: true,
+                required: "Type of exercise is required"
+            },
+
+            name: {
+                type: String,
+                trim: true,
+                required: "Name of exercise is required"
+            },
+
+            duration: {
+                type: Number,
+                required: "Duration is required"
+            },
+
+            distance: {
+                type: Number,
+            },
+
+            weight: {
+                type: Number,
+            },
+
+            sets: {
+                type: Number,
+            },
+
+            reps: {
+                type: Number
+            }
+
+        }
+
+    ]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+
+module.exports = Workout;
---
+++ models/index.js
@@ -0,0 +1,2 @@
+//in the activity ex 14 and miniproject why is it that in one they use index to export and in other models they dont use an index.js
+module.exports = { Workout: require('./Workout')};
